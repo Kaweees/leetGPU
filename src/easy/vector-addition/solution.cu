8a324a9d1941235b830b13ec10cf4fb169f04bd9
Kaweees <Kaweees@users.noreply.github.com>
2025-03-21 03:40:07 +0000

    Code formatting and linting

--- src/easy/vector-addition/solution.cu
+++ src/easy/vector-addition/solution.cu
@@ -1,17 +1,15 @@
 #include <cuda_runtime.h>
 
 __global__ void vector_add(const float* A, const float* B, float* C, int N) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < N) {
-        C[idx] = A[idx] + B[idx];
-    }
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  if (idx < N) { C[idx] = A[idx] + B[idx]; }
 }
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
 void solve(const float* A, const float* B, float* C, int N) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
-    cudaDeviceSynchronize();
+  vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+  cudaDeviceSynchronize();
 }
